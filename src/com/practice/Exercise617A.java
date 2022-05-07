1a82389cc58c008c9b05185d8bf738942cfa8597
shlmvgleb <glebshalimov2003@mail.ru>
2022-05-07 13:01:47 +0300

    new exercise

--- src/com/practice/Exercise617A.java
+++ src/com/practice/Exercise617A.java
@@ -1,7 +1,25 @@
 package com.practice;
 
+import java.util.*;
+
 public class Exercise617A {
     public static void main(String[] args) {
-        System.out.println("333");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int[] x1 = {1, 2, 3, 4, 5};
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i : x1) {
+            numbers.add(i);
+        }
+        int m = 0;
+        int min = numbers.get(numbers.size() - 1);
+        for (Integer number : numbers) {
+            if (x / number <= x) {
+                min = x / number;
+                m = number;
+            }
+        }
+        if(x % m > 0) System.out.println(min + 1);
+        else System.out.println(min);
     }
 }
