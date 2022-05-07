fe9b30a1ad669b0678007ab23712e056cc235d1b
shlmvgleb <glebshalimov2003@mail.ru>
2022-05-07 13:22:04 +0300

    new exercise

---
+++ src/com/practice/Exercise977A.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class Exercise977A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int i = 0; i < k; i++) {
+            int last = Integer.parseInt(String.valueOf(n).split("")[String.valueOf(n).split("").length - 1]);
+            if (last > 0) n -= 1;
+            else n /= 10;
+        }
+        System.out.println(n);
+    }
+}
