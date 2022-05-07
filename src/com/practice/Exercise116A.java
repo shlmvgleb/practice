a3d696384911576d6b817d09ade0e05f8c641679
shlmvgleb <glebshalimov2003@mail.ru>
2022-05-07 13:30:19 +0300

    new exercise

---
+++ src/com/practice/Exercise116A.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class Exercise116A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int max = 0;
+        int current = 0;
+        for (int i = 0; i < count; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            current -= a;
+            current += b;
+            if (current > max) max = current;
+        }
+        System.out.println(max);
+    }
+}
